e0c8afce9836e4550ce19ef6dbec9409983f933f
Templide <templide@gmail.com>
2020-04-15 15:52:44 +0900

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,7 +8,7 @@ module.exports = {
     instances: 1,
     autorestart: true,
     watch: true,
-    max_memory_restart: '2G',
+    max_memory_restart: '1G',
     env: {
       NODE_ENV: 'development'
     },
